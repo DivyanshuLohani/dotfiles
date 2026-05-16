f5c184416f4b615b928d71ee9d912f8de2e4b4ca
Divyanshu Lohani <divyanshu@divyanshulohani.xyz>
2026-05-16 23:35:21 +0530

    add installing stuff in install script

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,22 +1,55 @@
 return {
 	"nvim-telescope/telescope.nvim",
+
 	dependencies = {
-		"nvim-lua/plenary.nvim"
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-fzf-native.nvim",
 	},
+
 	config = function()
+		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
 
+		telescope.setup({
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules",
+					".git/",
+				},
+			},
+
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
+
+			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+				},
+			},
+		})
+
+		telescope.load_extension("fzf")
+
+		-- File navigation
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
+		-- LSP
 		vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
 		vim.keymap.set("n", "gr", builtin.lsp_references, {})
 		vim.keymap.set("n", "gi", builtin.lsp_implementations, {})
 		vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, {})
 		vim.keymap.set("n", "<leader>ws", builtin.lsp_workspace_symbols, {})
+
+		-- VSCode-like
 		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-		vim.keymap.set("i", "<C-p>", builtin.find_files, {})
 	end,
 }
