297b16d972c9df0127c39e2db9932d89881f046b
Divyanshu Lohani <divyanshu@divyanshulohani.xyz>
2026-05-16 23:35:21 +0530

    add installing stuff in install script

--- install.sh
+++ install.sh
@@ -8,10 +8,39 @@ echo "🚀 Setting up your environment..."
 # 1. Install basic packages
 # -------------------------
 echo "📦 Installing packages..."
-sudo apt update
-sudo apt install -y zsh git curl
 
-# -------------------------
+sudo apt update
+
+sudo apt install -y \
+  zsh \
+  git \
+  curl \
+  neovim \
+  ripgrep \
+  fd-find \
+  build-essential \
+  unzip \
+  gcc \
+  make \
+  python3-pip \
+  xclip \
+  tmux \
+  lazygit \
+  zoxide
+
+# fd alias for Ubuntu
+if ! grep -q "alias fd=fdfind" "$HOME/.zshrc"; then
+  echo 'alias fd=fdfind' >> "$HOME/.zshrc"
+fi
+
+# zoxide init
+if ! grep -q 'zoxide init zsh' "$HOME/.zshrc"; then
+  echo 'eval "$(zoxide init zsh)"' >> "$HOME/.zshrc"
+fi
+
+
+
+# ---- $DOTFILES-----------
 # 2. Install Oh My Zsh
 # -------------------------
 echo "⚡ Installing Oh My Zsh..."
@@ -50,6 +79,13 @@ ln -sf $DOTFILES/zsh/.zshrc $HOME/.zshrc
 ln -sf $DOTFILES/zsh/.p10k.zsh $HOME/.p10k.zsh
 ln -sf $DOTFILES/git/.gitconfig $HOME/.gitconfig
 
+mkdir -p $HOME/.local/share/konsole
+ln -sf $DOTFILES/konsole/Cyberdream.colorscheme \
+  $HOME/.local/share/konsole/Cyberdream.colorscheme
+
+mkdir -p $HOME/.config
+ln -sfn $DOTFILES/nvim $HOME/.config/nvim
+
 # -------------------------
 # 6. Set zsh as default
 # -------------------------
@@ -57,3 +93,4 @@ echo "🐚 Setting zsh as default shell..."
 chsh -s $(which zsh)
 
 echo "✅ Setup complete! Restart your terminal."
+echo "DO install yazi from yazi if you not know"
