2673b011e42250e5768e49f82b303cb96d6d7763
Divyanshu Lohani <divyanshu@divyanshulohani.xyz>
2026-05-27 23:41:30 +0530

    fix indent color

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,5 +1,18 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  opts = {},
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+
+	opts = {},
+
+	config = function(_, opts)
+		require("ibl").setup(opts)
+
+		vim.api.nvim_set_hl(0, "IblIndent", {
+			fg = "#3b4261",
+		})
+
+		vim.api.nvim_set_hl(0, "IblScope", {
+			fg = "#7aa2f7",
+		})
+	end,
 }
